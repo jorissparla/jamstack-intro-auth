2351d746861874d53110e000d9ef250aa9c67785
Joris Sparla <joris.sparla@gmail.com>
2020-03-08 14:52:04 +0100

    first commit

---
+++ src/components/route-base.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Link } from "gatsby";
+
+const RouteBase = () => <h1>All you base are belong to us</h1>;
+
+export default RouteBase;
---
+++ src/components/route-secret.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Link } from "gatsby";
+
+const RouteSecret = () => <h1>Super Scret Stuff</h1>;
+
+export default RouteSecret;
